245851d33c9df4048997b3df11b82d5672ef0b98
BrianDCF99 <bda59@sfu.ca>
2023-11-25 16:38:02 -0800

    Created JavaFX Project, and set up the files to be ready to code

---
+++ superhumans/src/main/java/com/example/App.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(new VBox(), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+   
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
